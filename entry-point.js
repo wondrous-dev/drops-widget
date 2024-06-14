023b5e7e2ad43ff3ab6fb14786b96171d3f5b9aa
Adrian <mustea.adrian95@gmail.com>
2024-06-14 18:02:35 +0300

    Update entry-point.js

--- entry-point.js
+++ entry-point.js
@@ -1,30 +1,109 @@
-(function (window) {
-  let e = document.createElement('iframe'),
-    t = document.getElementById('drops-widget-script'),
-    d = t.getAttribute('data-drops-widget');
-  (e.src = d),
-    (e.style.width = '60vw'),
-    (e.style.height = '60vh'),
-    (e.frameborder = '0'),
-    (e.style.border = '1px solid black'),
-    (e.style.borderRadius = '10px');
-  let l = document.createElement('div');
-  (l.id = 'drops-widget-container'),
-    (l.style.position = 'fixed'),
-    (l.style.width = 'fit-content'),
-    (l.style.top = '25%'),
-    (l.style.left = '50%'),
-    (l.style.padding = '20px'),
-    (l.style.transform = 'translate(-50%, 0)'),
-    l.appendChild(e),
-    document.body.appendChild(l);
-})(window);
+const detectClickOutside = (el, button, callback) => {
+  const handleClickOutside = (event) => {
+    if (!el.contains(event.target) && !button.contains(event.target)) {
+      callback();
+    }
+  };
+  document.addEventListener('click', handleClickOutside);
+  return {
+    destroy() {
+      document.removeEventListener('click', handleClickOutside);
+    },
+  };
+};
 
-/*
-    USAGE: 
-   <script
-      id="drops-widget-script"
-      data-drops-widget="<ENTER DROPS APP URL LINK HERE WITH THE EXTERNAL ID. e.g https://app.drops.house/?ext_id=abc>"
-      src"<ENTER SCRIPT URL HERE>"
-    ></script>
-*/
+const renderIframe = (url, container) => {
+  const iframeExists = container.querySelector('#drops-widget-iframe');
+  if (iframeExists) {
+    container.style.display = 'block';
+    return;
+  }
+  const iframe = document.createElement('iframe');
+  iframe.id = 'drops-widget-iframe';
+  document.body.style.overflow = 'hidden';
+  document.body.style.height = '100vh';
+  container.style.position = 'fixed';
+  container.style.top = '0';
+  container.style.left = '0';
+
+  iframe.src = url;
+  container.style.width = '100vw';
+  container.style.height = '100vh';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  iframe.style.borderRadius = '10px';
+  iframe.style.transformOrigin = 'top left';
+  container.appendChild(iframe);
+};
+
+const removeIframe = (container) => {
+  const iframe = document.querySelector('#drops-widget-iframe');
+  if (iframe) {
+    container.style.display = 'none';
+
+    document.body.style.overflow = 'auto';
+    document.body.style.height = 'auto';
+  }
+};
+
+const createButtonElement = () => {
+  const button = document.createElement('button');
+  button.style.position = 'fixed';
+  button.style.top = 'calc(100% - 100px)';
+  button.style.left = 'calc(100% - 100px)';
+  button.style.borderRadius = '50%';
+  button.style.border = '1px solid #000';
+  button.style.backgroundColor = '#000';
+  button.style.width = '60px';
+  button.style.height = '60px';
+  button.style.padding = '5px';
+  button.style.display = 'flex';
+  button.style.justifyContent = 'center';
+  button.style.alignItems = 'center';
+  button.style.cursor = 'pointer';
+  return button;
+};
+
+const renderWidget = () => {
+  let isIframeRendered = false;
+  const script = document.getElementById('drops-widget-script');
+
+  const button = createButtonElement();
+  const img = document.createElement('img');
+  img.src = 'https://omni.drops.house/logo-2.svg';
+  img.style.height = '50px';
+  img.style.width = '50px';
+  button.appendChild(img);
+
+  const container = document.createElement('div');
+  container.style.overflow = 'hidden';
+  container.id = 'drops-widget-container';
+  const url = script.getAttribute('data-drops-widget');
+
+  document.body.appendChild(container);
+  detectClickOutside(container, button, () => {
+    isIframeRendered = !isIframeRendered;
+    removeIframe(container);
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (isIframeRendered) {
+      removeIframe(container);
+    } else {
+      renderIframe(url, container);
+    }
+
+    isIframeRendered = !isIframeRendered;
+  };
+
+  button.addEventListener('click', handleClick);
+  document.body.appendChild(button);
+};
+
+// CSS for animations
+const style = document.createElement('style');
+
+document.head.appendChild(style);
+renderWidget();
